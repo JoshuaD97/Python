2cc1ecc59b81eb97c66e025980336553d2662f71
JoshuaD97 <diazjosh66@gmail.com>
2024-01-28 16:21:49 -0600

    Indexing ps script

--- Index.py
+++ Index.py
@@ -3,3 +3,15 @@
 'this is also a string'
 print("hello \n world")
 len('I am')
+
+mystring = "Hello World"
+mystring[-1]
+
+mystring2 = 'asdfeghijk'
+mystring2[2:]
+mystring2[:3]
+mystring2[3:6]
+mystring2[0]
+mystring[::3]
+mystring[::-1]
+'Hello World'[3]
