0c6a5453e1050fb8b1fa6a8f558c83f72fe44233
JoshuaD97 <diazjosh66@gmail.com>
2024-01-31 17:22:51 -0600

    Added two files.

---
+++ List.py
@@ -0,0 +1,25 @@
+my_list = [1,2,3]
+my_list = ['STRING',100,23.2]
+len(my_list)
+my_list[0:]
+
+another_list = ['four', 'five']
+new_list = my_list + another_list
+new_list[0] = 'ONE'
+new_list.append('six')
+new_list.remove('four')
+new_list.pop()
+new_list
+new_list.pop(-1)
+
+new_list = ['a','b','x','d','e']
+new_list.sort()
+new_list
+
+new_list.sort()
+my_sorted_list = new_list
+my_sorted_list
+
+num_list = [4,1,8,3]
+num_list.reverse()
+num_list
---
+++ PrintFormat.py
@@ -0,0 +1,16 @@
+print('This is a string {}'.format('INSERTED'))
+print('The {2} {1} {0}'.format('fox','brown','quick'))
+print('The {q} {b} {f}'.format(f='fox',b='brown',q='quick'))
+
+result = 100/777
+print("The result was {r}".format(r=result))
+
+result = 100/777
+print("The result was {r:1.3f}".format(r=result))
+
+name = "Jose"
+print(f'Hello, his name is {name}')
+
+name = "Sam"
+age = 3
+print(f'{name} is {age} years old.')
